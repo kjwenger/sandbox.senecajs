f4184b2d53ba80ca02c1f01182f80c505af0bebf
Klaus Wenger <kjwenger@yahoo.com>
2017-10-14 18:42:56 +0200

    first real successes

--- ts/microservices/tomler.ts
+++ ts/microservices/tomler.ts
@@ -1,8 +1,8 @@
 export function tomler () {
-  this.add('role:convert,cmd:from', (msg: any, respond: any) => {
+  this.add('role:convert-toml,cmd:from', (msg: any, respond: any) => {
     respond(`${msg}`)
   })
-  this.add('role:convert,cmd:to', (msg: any, respond: any) => {
+  this.add('role:convert-toml,cmd:to', (msg: any, respond: any) => {
     respond(`${msg}`)
   })
 }
